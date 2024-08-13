a11fe09dc2883d1ab6afa031e130d3f4bdaafdd1
brunolmars <bruno.lmarques@sempreceub.com>
2024-08-13 09:27:32 -0300

    h

---
+++ script.js
@@ -0,0 +1,17 @@
+let dom =document;
+
+let titulo = document.getElementById("titulo").value;
+
+console.log(dom);
+
+
+console.log(titulo);
+
+function submeter(){
+    let nome = document.getElementById("nome").value;
+    console.log(nome)
+
+}
+
+
+
