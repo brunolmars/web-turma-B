079824792f6055fc347150aad13ad4e2033a26ea
brunolmars <bruno.lmarques@sempreceub.com>
2024-08-13 09:40:28 -0300

    gh

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 let dom =document;
+console.log(dom);
 
 let titulo = document.getElementById("titulo").value;
-
-console.log(dom);
+console.log(titulo);
 
 
 console.log(titulo);
 
 function submeter(){
     let nome = document.getElementById("nome").value;
-    console.log(nome)
+    let idade = document.getElementById("idade").value;
+    let email = document.getElementById("email").value;
+    console.log(nome);
+    console.log (valida_idade(idade))
+
+}
+function valida_idade(idade){
+
+    if (idade=""){
+        alert ("esse campo não pode ser vazio")
+        return false 
+    }
+    if(idade = String){
+    alert("não pode colocar letra")
+return false
+    }
+    return true
+
+
 
 }
 
